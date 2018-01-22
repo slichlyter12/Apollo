cb465044bdec8cc94c5a39a803815d2cb17e9c49
Sam Lichlyter <slichlyter12@me.com>
2018-01-22 13:41:56 -0800

    added checkout

--- pull.py
+++ pull.py
@@ -1,12 +1,33 @@
 #!/usr/local/bin/python3.6
 
+import os
+import subprocess
+import re
 import csv
+from git import Repo
 
 IMPORT_FILE = 'usernames.csv'
-FIELDNAMES = ['firstname', 'lastname', 'id', 'url']
+FIELDNAMES = ['firstname', 'lastname', 'gid', 'osuid', 'url']
+ASSIGNMENT_NUM = '1'
 
 with open(IMPORT_FILE, newline='') as infile:
-    reader = csv.DictReader(infile, fieldnames=FIELDNAMES, dialect='excel')
+    reader = csv.DictReader(infile, fieldnames=FIELDNAMES)
     next(reader) # Skip over header
     for row in reader:
-        print(row['firstname'], row['lastname'], row['id'], row['url'])
+
+        # Get repo and branch
+        repo_name = row['url']
+        branch_name = row['osuid'] + '-assignment-' + ASSIGNMENT_NUM
+
+        # Check if repo exists, if so pull, if not clone
+        if not (os.path.exists(row['osuid'])):
+            # Clone
+            os.mkdir(row['osuid'])
+            Repo.clone_from(repo_name, row['osuid'])
+
+        # Switch to desired branch
+        os.chdir(row['osuid'])
+        repo = Repo()
+        git = repo.git
+        git.checkout(branch_name)
+
